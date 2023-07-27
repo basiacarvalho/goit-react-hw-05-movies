6208853c5baf64744cb262d8df90fc5313c86389
basiacarvalho <basiacarvalho@gmail.com>
2023-07-27 14:50:48 +0200

    adding routes to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import { lazy } from 'react';
 
 const Home = lazy(() => import('./pages/home/Home'));
 const Movies = lazy(() => import('./pages/movies/Movies'));
+const MovieDetails = lazy(() => import('./pages/moviedetails/MovieDetails'));
+const Cast = lazy(() => import('./pages/cast/Cast'));
+const Reviews = lazy(() => import('./pages/reviews/Reviews'));
 
 export const App = () => {
   return (
@@ -11,6 +14,9 @@ export const App = () => {
       <Route path="/" element={<CommonLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} />
+        <Route path="movies/:movieId/cast" element={<Cast />} />
+        <Route path="movies/:movieId/reviews" element={<Reviews />} />
       </Route>
     </Routes>
   );
