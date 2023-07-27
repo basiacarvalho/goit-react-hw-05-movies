dd998de47c6f65d974c84dca97fee0c876df6ac4
basiacarvalho <basiacarvalho@gmail.com>
2023-07-27 21:12:02 +0200

    rendering the list of trending movies from API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,28 @@
 import css from './Home.module.css';
+import { fetchTrendingMovies } from 'utils';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const movies = await fetchTrendingMovies();
+      setTrendingMovies(movies);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className={css.trendingContainer}>
         <h2 className={css.trendingTitle}>Trending today</h2>
         <ul className={css.trendingList}>
-          <li className={css.trendingListItem}>Moj trending movie</li>
+          {trendingMovies.map(el => (
+            <li key={el.id} className={css.trendingListItem}>
+              {el.title}
+            </li>
+          ))}
         </ul>
       </div>
     </main>
---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const options = {
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzVlYmZjOGZkMWM2MWQ5MWJlN2FlNTVhMGVmM2E2MSIsInN1YiI6IjY0ODFiNWNiNjQ3NjU0MDEyNDk3MGQzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3215WRy5xLM7EaHoA7mSNicq_FEcMJVAtrDLqSZgqNM',
+  },
+};
+
+export const fetchTrendingMovies = async () => {
+  const result = await axios.get(
+    'https://api.themoviedb.org/3/trending/movie/day',
+    options
+  );
+
+  const results = result.data.results;
+  const movieList = results.map(movie => {
+    return {
+      id: movie.id,
+      title: movie.title,
+    };
+  });
+  return movieList;
+};
